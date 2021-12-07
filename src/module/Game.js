bc3d0a03fb44d291b8c6f478d6452401c81920f1
mba_m1_silver <ucn.dev@gmail.com>
2021-12-07 10:50:15 +0900

    勝敗判定（二次元ve）

--- src/module/Game.js
+++ src/module/Game.js
@@ -26,23 +26,39 @@ class Game extends React.Component {
   }
 
   calculateWinner(squares) {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        return squares[a];
+    const lines_num = (SQUARE_NUM * 2) + 1 // 縦横の行数＋斜め（斜めは１回のループでクロス両方を作るので、+1）
+    const lines = [...squares]
+    
+    for(let i = 0; i < lines_num; i++) {
+      // 横
+      if(i < SQUARE_NUM) {
+      }
+      // 縦
+      else if(i >= SQUARE_NUM && i < (SQUARE_NUM*2)) {
+        const column_num = (i - SQUARE_NUM)
+        lines.push(squares.map(row => row[column_num]))
+      }
+      // 斜め
+      else {
+        // 斜めの左右
+        Array(2).fill(null).map((a, direction) => {
+          const vector = direction == 0 ? 1 : -1
+          lines.push(squares.map((row, i) => row[(((SQUARE_NUM-1)*direction) + (i*vector))]))
+        })
       }
     }
-    return null;
+
+    console.log(lines)
+
+    // 判定プロセス --------------------------
+    let result = null
+    lines.some(line => {
+      const game_over = line.every(v => v === line[0])
+      if(game_over) { result = line[0] }
+      return game_over
+    })
+
+    return result
   }
 
   jumpTo(step){
@@ -65,9 +81,9 @@ class Game extends React.Component {
     const history = this.state.history.slice(0,this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = [...current.squares]
-    // if (this.calculateWinner(squares) || squares[idx]){
-    //   return;
-    // }
+    if (this.calculateWinner(squares) || squares[idx]){
+      return;
+    }
     squares[row][column] = this.state.xIsNext ? 'X' : '0';
     this.setState({
       history: history.concat([{
