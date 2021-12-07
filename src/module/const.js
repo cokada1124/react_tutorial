faf2daf0b6c18a4dd4234e81f51e9c46c33c2de1
ucn.dev_spacegrey <ucn.dev@gmail.com>
2021-12-07 09:07:48 +0900

    コード漏れ追加

--- src/module/const.js
+++ src/module/const.js
@@ -18,7 +18,8 @@ line.map((v, i) => {
 })
 
 
-for (let j = 0; j < 9; j++) {
+for (let i = 0; i < 9; i++) {
+  for (let j = 0; j < 9; j++) {
   console.log("[" + i +"]" + "[" +j+ "]");
   console.log("[" + j +"]" + "[" +i+ "]");
   }
